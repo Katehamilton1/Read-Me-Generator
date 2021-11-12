f6b9d30c13ae895029b6aecd9c5dbf229f88488a
Kate Hamilton <kate.hamilton828@gmail.com>
2021-11-12 06:33:53 -0500

    license and mark down

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: `what is the title of your repository`,
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter your repository title.(Required)');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'What is the description of your repository? (Required)',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            } else {
+                console.log('Please enter a description of the repository.');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: `Whats your GitHub user Name?`,
+    },
+
+    {
+        type: 'confirm',
+        name: 'install',
+        message: `Do you want to add any installation notes?`,
+    },
+    {
+        type: 'rawlist',
+        name: 'license',
+        message: 'Which open source license would you like to use?',
+        choices: [
+            'MIT',
+            'APACHE',
+            'GPL',
+            'Boost Software License 1.0',
+            'BSD 3-Clause License'
+        ]
+    },
+    {
+        type: 'confirm',
+        name: 'credits',
+        message: `Would you like to add any credits to the repo?`,
+    },
+
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
+let content = generateMarkdown(data);
+fs.writeFile(fileName, content, function (error) {
+    if (error) {
+        return console.log(error)
+    }
+    console.log('success')
+});
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
+inquirer.prompt(questions).then(function (data) {
+    var fileName = 'README.md';
+    writeToFile(fileName, data)
+});
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,82 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+const licenseArr = ['MIT', 'APACHE', 'Boost Software License 1.0', 'BSD 3-Clause License']
+
+
+function renderLicenseBadge(license) {
+  if (license === licenseArr[0]) {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+  } else if (license === licenseArr[1]) {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+  } else if (license === licenseArr[2]) {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)"
+  } else if (license === licenseArr[3]) {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+  } else {
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === licenseArr[0]) {
+    return `[${licenseArr[22]}](https://opensource.org/licenses/MIT)`
+  } else if (license === licenseArr[1]) {
+    return `[${licenseArr[1]}](https://opensource.org/licenses/Apache-2.0)`
+  } else if (license === licenseArr[2]) {
+    return `[${licenseArr[3]}](https://www.boost.org/LICENSE_1_0.txt)`
+  } else if (license === licenseArr[3]) {
+    return `[${licenseArr[5]}](https://opensource.org/licenses/BSD-3-Clause)`
+  } else {
+    return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license === licenseArr[0]) {
+    return `Read more about ${licenseArr[0]} here:`
+  } else if (license === licenseArr[1]) {
+    return `Read more about ${licenseArr[1]} here:`
+  } else if (license === licenseArr[2]) {
+    return `Read more about ${licenseArr[2]} here:`
+  } else if (license === licenseArr[3]) {
+    return `Read more about ${licenseArr[3]} here:`
+  } else {
+    return ''
+  }
+}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ## Badges
+  ${renderLicenseBadge(data.license)}
+
+
+## Table of Contents
+* [title](#title)
+* [Description](#description)
+* [userName](#userName)
+* [Installation](#installation)
+* [License](#license)
+
+
+
+## Description
+${data.description}
+
+##Instalation
+${data.install}
+
+
+
+## Licences 
+${renderLicenseSection(data.license)}
+${renderLicenseLink(data.license)}
+
 
 `;
 }
